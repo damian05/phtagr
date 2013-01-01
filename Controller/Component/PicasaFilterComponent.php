dbd5c5fb14f1f204c519a3f579df75c1a0438ee0
Damian <damianpublic@yahoo.fr>
2013-01-01 16:50:27 +0200

    Improve PicasaFilter.

--- Controller/Component/PicasaFilterComponent.php
+++ Controller/Component/PicasaFilterComponent.php
@@ -21,6 +21,7 @@ App::uses('BaseFilter', 'Component');
 class PicasaFilterComponent extends BaseFilterComponent {
   var $controller = null;
   var $components = array('FileManager');
+  var $picasaContactsCache = array();
 
   public function getName() {
     return "Picasa";
@@ -30,48 +31,90 @@ class PicasaFilterComponent extends BaseFilterComponent {
     return array('ini' => array('priority' => 4));
   }
 
-  public function read($file, $media = null){
-    
-    $filename = $this->MyFile->getFilename($file);
-    
+  /**
+   * Read picasa contacts.xml file.
+   *
+   * @return array of Picasa contacts
+   */
+  private function _readPicasaContacts() {
     //path to picasa contacts.xml file
-    $xml = Xml::build('/media/black/FOTO/2012 test picasa/picasa contacts/contacts.xml');   
-    
-    //http://book.cakephp.org/2.0/en/core-utility-libraries/xml.html
-    $xmlArray = Xml::toArray($xml);
-    $contacts = $xmlArray['contacts']['contact'];
-    
+    $path = '/media/black/FOTO/2012 test picasa/picasa contacts/contacts.xml';
+
+    if (!isset($this->picasaContactsCache[$path])) {
+      $xml = Xml::build($path);   
+      //http://book.cakephp.org/2.0/en/core-utility-libraries/xml.html
+      $xmlArray = Xml::toArray($xml);
+      if (isset($xmlArray['contacts']['contact'])) {
+        $this->picasaContactsCache[$path] = $xmlArray['contacts']['contact'];
+      } else {
+        $this->picasaContactsCache[$path] = false;
+      }
+    }
+    return $this->picasaContactsCache[$path];
+  }
+  
+  public function read($file, $media = null){
+    $filename = $this->MyFile->getFilename($file);
+
+    $readed = strtotime($file['File']['readed']);
+
+    //TODO: change FilterManager in order to skip files readed, not changed and without media?? and delete next section
+    // Check changes
+    $fileTime = filemtime($filename);
+    $dbTime = strtotime($file['File']['time']);
+    //Logger::debug("db $dbTime file $fileTime");
+    $forceRead = false;
+    if ($fileTime > $dbTime) {
+      Logger::warn("File '$filename' was changed without notice of phTagr! Read the file again.");
+      // @todo Action if file is newer than phtagr. Eg. force rereada
+      $forceRead = true;
+    }
+    if ($readed && !$forceRead) {
+      Logger::verbose("File '$filename' already readed. Skip reading!");
+      $this->FilterManager->skipped[$filename] = 'skipped';
+      //return $media;//overload memory with 20kb for each file
+      return $filename;//around 0.2kb
+    }
+
+    $contacts = $this->_readPicasaContacts();
     $picasaFiles = $this->_readPicasaIni($filename, $contacts);
-    
+    if (!$contacts || !count($picasaFiles)) {
+      return false;
+    }
+    $path = $file['File']['path'];
+
     foreach ($picasaFiles as $picasaFile=>$faces) {
-      $path = $file['File']['path'];
       $media = $this->FilterManager->_findMediaInPath($path, $path.$picasaFile);
       if (!$media) {
         continue;
       }
-      $facesnames = array();
+
+      $picasaFacesNames = array();
       foreach ($faces as $face) {
         if (isset($face['name'])) {
           //keep only faces with names in contacts.xml, i.e. known faces
-          $facesnames[]= $face['name'];
+          $picasaFacesNames[]= $face['name'];
         }
       }
-      $values = array_unique($facesnames);
-      $media['Field']['face'] = $values;
-      
-      if (!$this->Media->save($media)) {
-        Logger::err("Could not save Media");
-        Logger::trace($media);
-        $this->FilterManager->addError($filename, 'MediaSaveError');
-      } else {
-        Logger::verbose("Updated media (id ".$media['Media']['id'].")");
+      $picasaFacesNamesUnique = array_unique($picasaFacesNames);
+      $dbfaces = Set::extract("/Field[name=face]/data", $media);
+      if (count(array_diff($picasaFacesNamesUnique, $dbfaces)) or count(array_diff($dbfaces, $picasaFacesNamesUnique))) {
+        $media['Field']['face'] = $picasaFacesNamesUnique;
+
+        if (!$this->Media->save($media)) {
+          Logger::err("Could not save Media");
+          Logger::trace($media);
+          $this->FilterManager->addError($path.$picasaFile, 'MediaSaveError');
+        } else {
+          Logger::verbose("Updated media (id ".$media['Media']['id'].")");
+        }
       }
-     $importLog = $this->FilterManager->_importlog($importLog, $path.$picasaFile);
+      $importLog = $this->FilterManager->_importlog($importLog, $path.$picasaFile);
     }
     
     $this->controller->MyFile->updateReaded($file);
     //$this->controller->MyFile->setFlag($file, FILE_FLAG_DEPENDENT);
-
+    return $filename;
   }
 
   private function _readPicasaIni($picasaIni, $contacts){
@@ -90,6 +133,7 @@ class PicasaFilterComponent extends BaseFilterComponent {
         $faces = split( ';', $faces);
         foreach ($faces as $face) {
           $face = split( ',', $face);
+          $crtface = array();
           $crtface['id'] = $face[1];
           $crtface['rect64'] = substr($face[0],7,strlen($face[0])-8);
           foreach ($contacts as $contact) {
